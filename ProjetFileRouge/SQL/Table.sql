5be58f74d3473c55de0deae61fb0d78e627a64e5
Michael Helinckx <minkus59@hotmail.com>
2021-05-04 11:38:33 +0200

    Ajout Table SQL

---
+++ ProjetFileRouge/SQL/Table.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE [dbo].[Utilisateur]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[Pseudo] NCHAR(10) NOT NULL,
+    [Nom] VARCHAR(80) NOT NULL, 
+    [Prenom] VARCHAR(80) NOT NULL, 
+    [Email] VARCHAR(MAX) NOT NULL, 
+    [Mdp] VARCHAR(80) NOT NULL, 
+    [DateCreation] TIMESTAMP NOT NULL, 
+    [Avatar] VARCHAR(MAX) NULL, 
+    [Actif] INT NOT NULL DEFAULT 0,
+    [Admin] INT NOT NULL DEFAULT 0
+)
+
+CREATE TABLE [dbo].[Canal]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Theme] VARCHAR(80) NOT NULL, 
+    [Description] VARCHAR(MAX) NULL, 
+    [DateCreation] TIMESTAMP NOT NULL, 
+    [Actif] INT NOT NULL DEFAULT 0,
+    [IdUtilisateur] INT NULL
+)
+
+CREATE TABLE [dbo].[Publication]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Publication] VARCHAR(80) NOT NULL, 
+    [DateCreation] TIMESTAMP NOT NULL, 
+    [Actif] INT NOT NULL DEFAULT 0,
+    [IdUtilisateur] INT NULL,
+    [IdCanal] INT NULL
+)
